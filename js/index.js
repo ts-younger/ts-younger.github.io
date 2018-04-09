02860621750305f6c99ee566ba0c2ee6161a5d4a
ts-younger <397041437@qq.com>
2018-04-09 21:10:18 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,18 +1,17 @@
 // jquery
 $(document).ready(function(){
 	var btnshow = false;
-	var innerWith = $(window).innerWidth();
-	$('.header-nav-btn').on('click', function() {
-		if(btnshow) $('.header-nav').hide();
-			else $('.header-nav').show();
-		btnshow = !btnshow;
-		event.stopPropagation()
-	})
-	$(document).on('click', function () {
-		$('.header-nav').hide()
-		btnshow = false
-	})
-	$('.header-nav li').on('click', function() {
-		// btnshow = false
-	})
+	var innerWidth = $(window).innerWidth();
+	if(innerWidth < 768 ) {
+		$('.header-nav-btn').on('click', function() {
+			if(btnshow) $('.header-nav').hide();
+				else $('.header-nav').show();
+			btnshow = !btnshow;
+			event.stopPropagation()
+		})
+		$(document).on('click', function () {
+			$('.header-nav').hide()
+			btnshow = false
+		})
+	}	
 })
